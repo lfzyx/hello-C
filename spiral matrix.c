c8d041713d6b7913e334af87fe7fd18c569d3224
lfzyx <lfzyx.me@gmail.com>
2015-01-14 12:43:38 +0800

    将do while循环改成for循环，前面加if else判断

--- spiral matrix.c
+++ spiral matrix.c
@@ -1,34 +1,33 @@
 //
-//  main.c
+//  spiral matrix.c
 //  hello c
 //
-//  Created by lfzyx on 14/12/1.
+//  Created by lfzyx on 15/1/14.
 //  Copyright (c) 2014年 lfzyx. All rights reserved.
 //
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #define N 9
-void pri(int shuzu[][N], int a, int b);//打印数组
-void around(int shuzu[][N], int x);//螺旋矩阵数组
+void pri(int Array[][N], int a, int b);
+void spiral(int spiral[][N], int x);
 int main()
 {
-
-    int shuzu[N][N];
+    int shuzu[N][N];//矩阵数组
     int a, b;//边长
     do
     {
-    printf("求边长不大于9的螺旋矩阵\n");
-    scanf("%d",&a);
-    }while (a>9||a<1);
-    around(shuzu, a);//赋数组初值
-    b=a;
-    pri(shuzu,a,b);//打印数组
+        printf("求边长不大于9的螺旋矩阵\n");
+        scanf("%d",&a);
+    } while (a > 9 || a < 1);
+    spiral(shuzu, a);//赋数组初值
+    b = a;
+    pri(shuzu, a, b);//打印数组
     return 0;
 }
 
-void around(int shuzu[][N], int x)
+/**
+ 螺旋矩阵数组，要传入变长x
+ **/
+void spiral(int spiral[N][N], int x)
 {
     int level = (x+1)/2;//算矩阵圈数公式
     int m=0;
@@ -41,54 +40,53 @@ void around(int shuzu[][N], int x)
         int left;
         x--;
         
-        top = m;
-        do
+        if (m==x)//计算矩形终点，最后一个值
         {
-            shuzu[m][top]=len;
-            //printf("上%d ",shuzu[m][top]);
-            len++;
-            top++;
-        }while (top < x);
-        
-        right = m;
-        do
-        {
-            shuzu[right][top]=len;
-           // printf("右%d ",shuzu[right][top]);
-            len++;
-            right++;
-        }while (right < x);
-        
-        under = right;
-        do
-        {
-            shuzu[right][under]=len;
-           // printf("下%d ",shuzu[right][under]);
-            len++;
-            under--;
-        }while (under > m);
+            spiral[m][x]=len;
             
-        left = right;
-        do
+        }
+        else//计算各边
         {
-            shuzu[left][under]=len;
-            //printf("左%d ",shuzu[left][under]);
-            len++;
-            left--;
-        }while(left > m);
-        
+            for(top = m;top < x;top++)
+            {
+                spiral[m][top]=len;
+                //printf("上%d ",shuzu[m][top]);
+                len++;
+            }
+            
+            for(right = m; right < x; right++)
+            {
+                spiral[right][top]=len;
+                // printf("右%d ",shuzu[right][top]);
+                len++;
+            }
+            
+            for(under = right;under > m;under--)
+            {
+                spiral[right][under]=len;
+                // printf("下%d ",shuzu[right][under]);
+                len++;
+            }
+            
+            for(left = right;left > m;left--)
+            {
+                spiral[left][under]=len;
+                //printf("左%d ",shuzu[left][under]);
+                len++;
+            }
+        }
         m++;
     }
 }
 
-void pri(int shuzu[][N], int a, int b)
+void pri(int Array[N][N], int a, int b)
 {
     int x, y;
     for (x=0; x<a; x++)
     {
         for (y=0; y<b; y++)
         {
-            printf("%d\t",shuzu[x][y]);
+            printf("%d\t",Array[x][y]);
         }
         printf("\n");
     }
